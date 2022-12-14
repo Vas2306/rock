a394291f2ca3181505af3663daa52b61b8bad1db
Vassa <vasilisa230679@gmail.com>
2022-12-14 21:57:57 +0100

    Использование функции в игре для исключения дублирования

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Рандомное число: ' + randomNumber);
 
-let computerMove = 'ход компьютера';
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'камнем';
+    }
+    if(argMoveId == 2){
+        return 'бумагой';
+      }
+      if(argMoveId == 3){
+        return 'ножницами';
+      } else {
+    printMessage('Ничего неизвестно о ходе: ' + argMoveId + '.');
+    return 'неизвестный ход';    
+    }   
+  }
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('Рандомное число: ' + randomNumber);
+  
+  let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
   computerMove = 'камень';
 }
 if(randomNumber == 2){
     computerMove = 'бумага';
   }
-  if(randomNumber == 3){
+if(randomNumber == 3){
     computerMove = 'ножницы';
-  }  
+  }*/  
 let playerInput = prompt('Выбирай свой ход! 1: камень, 2: бумага, 3: ножницы.');
 
 console.log('Игрок вписал: ' + playerInput);
 
-let playerMove = 'неизвестный ход';
+let playerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
+/*if(playerInput == '1'){
   playerMove = 'камень';
 }
 if(playerInput == '2'){
@@ -26,10 +41,49 @@ if(playerInput == '2'){
   }
   if(playerInput == '3'){
     playerMove = 'ножницы';
-  }
-printMessage('Твой ход - это: ' + playerMove);
+  } else {
+    playerMove = 'неизвестный ход';
+  }*/
+
+  function displayResult(argComputerMove, argPlayerMove){
+    printMessage('Я играл ' + argComputerMove + ', a ты - ' + argPlayerMove);
+    if(argComputerMove == 'камнем' && argPlayerMove == 'бумагой'){
+        printMessage('Ты выиграл!');
+      }
+    if(argComputerMove == 'ножницами' && argPlayerMove == 'камнем'){
+        printMessage('Ты выиграл!');
+      } 
+    if(argComputerMove == 'бумагой' && argPlayerMove == 'ножницами'){
+        printMessage('Ты выиграл!');
+      }
+    if(argComputerMove == 'бумагой' && argPlayerMove == 'камнем'){
+        printMessage('Ты проиграл!');
+      }  
+    if(argComputerMove == 'камнем' && argPlayerMove == 'ножницами'){
+        printMessage('Ты проиграл!');
+      } 
+    if(argComputerMove == 'ножницами' && argPlayerMove == 'бумагой'){
+        printMessage('Ты проиграл!')
+      }
+    if(argComputerMove == 'ножницами' && argPlayerMove == 'ножницами'){
+        printMessage('Ничья!');
+      }      
+    if(argComputerMove == 'камнем' && argPlayerMove == 'камнем'){
+        printMessage('Ничья!');
+      }  
+    if(argComputerMove == 'бумагой' && argPlayerMove == 'бумагой'){
+        printMessage('Ничья!');
+      }
+    }
+
+
+let result = displayResult(computerMove, playerMove);
+
+/*printMessage('Твой ход - это: ' + playerMove);
 
 printMessage('Мой ход - это ' + computerMove);
+
+
 if( computerMove == 'камень' && playerMove == 'бумага'){
     printMessage('Ты выиграл!');
   }
@@ -56,4 +110,4 @@ if( computerMove == 'камень' && playerMove == 'камень'){
   }  
 if( computerMove == 'бумага' && playerMove == 'бумага'){
     printMessage('Ничья!');
-  }
+  }*/
